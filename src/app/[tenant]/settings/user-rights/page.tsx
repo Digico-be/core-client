7521bb0432c51136f4815e580adcd21e44fdacef
Q210099 <j.rezette@student.helmo.be>
2025-03-26 13:04:12 +0100

    initial commit this branch are link with branch Task-2

--- src/app/[tenant]/settings/user-rights/page.tsx
+++ src/app/[tenant]/settings/user-rights/page.tsx
@@ -28,10 +28,10 @@ export default function Page() {
             <Grid.Col>
                 <div className="flex justify-between items-center">
                     <PageHeader>Droits utilisateurs</PageHeader>
-                    {/*<div className="flex gap-2">
-                        <QuerySearchBar />
+                    <div className="flex gap-2">
+                        {/*<QuerySearchBar />*/}
                         <Button href="/settings/users/create">Ajouter un utilisateur</Button>
-                    </div>*/}
+                    </div>
 
                 </div>
             </Grid.Col>
